23e71228f3d25026b5e529a0be84db7c5068817e
akelly223 <abdoulayekelly223@gmail.com>
2026-07-16 09:01:35 +0000

    feat(students): migration EF pour les nouveaux champs etudiant

---
+++ Migrations/20260716085133_AjoutDetailsEtudiant.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionScolaire.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjoutDetailsEtudiant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateNaissance",
+                table: "Students",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Students",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateNaissance",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Students");
+        }
+    }
+}
